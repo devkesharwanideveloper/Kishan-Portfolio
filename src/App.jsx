aa9a7806d70df2192c54ab5eccc23d8fbd9a92be
devkesharwanideveloper <devkesharwani508@gmail.com>
2025-12-27 01:31:54 +0530

    addTailwindCssTest

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
    <>
-   <div>Home Page</div>
+   <div className="bg-red-500">Home Page</div>
    </>
   )
 }
